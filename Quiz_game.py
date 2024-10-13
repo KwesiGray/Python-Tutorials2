fca6586832ff763d40494bdcda0297c1b47f6cd9
David Graham <cypherrix5@icloud.com>
2024-10-13 22:57:49 +0000

    2nd_Commit

--- Quiz_game.py
+++ Quiz_game.py
@@ -28,28 +28,122 @@ if agree.lower() == "yes":
     print("\n")
     print("Your Details are: \n" + "Name: " + name + "\n" + "Age: " + str(age) + "\n" + "Gender: " + gender)
 
+
+name.capitalize()
+gender.capitalize()
+score = 0
+Q_no = 0
+
+
+
 print("\n")
 print("Let's get started! ")
 print("\n")
 
-answer = input("What's the capital of France? ")
+Q_no += 1
+answer = input(f"{Q_no}. What's the capital of France? ")
 if answer.lower() == "paris":
     print("Correct! ")
+    score += 1
 else:
     print("Incorrect! ")
+    
     
 
 print("\n")
 print("Next Question! ")
 print("\n")
 
-answer = input("What's the full meaning of CPU? ")
-if answer.lower() == "Central Processing Unit":
+Q_no += 1
+answer = input(f"{Q_no}. What's the full meaning of CPU? ")
+if answer.lower() == "central processing unit":
     print("Correct! ")
+    score += 1
+else:
+    print("Incorrect! ")
+    
+    
+    
+
+print("\n")
+print("Next Question! ")
+print("\n")
+Q_no += 1
+answer = input(f"{Q_no}. What is the capital of Ghana? ")
+if answer.lower() == "accra":
+    print("Correct! ")
+    score += 1
+else:
+    print("Incorrect! ")
+    
+    
+
+
+
+print("\n")
+print("Next Question! ")
+print("\n")
+Q_no += 1
+answer = input(f"{Q_no}. What's the largest organ in the human body? ")
+if answer.lower() == "liver":
+    print("Correct! ")
+    score += 1
+else:
+    print("Incorrect! ")
+    
+    
+
+
+
+print("\n")
+print("Next Question! ")
+print("\n")
+Q_no += 1
+answer = input(f"{Q_no}. What does RAM stand for? ")
+if answer.lower() == "random access memory":
+    print("Correct! ")
+    score += 1
+else:
+    print("Incorrect! ")
+    
+    
+
+
+
+print("\n")
+print("Next Question! ")
+print("\n")
+Q_no += 1
+answer = input(f"{Q_no}. Sound is measured in what unit? ")
+if answer.lower() == "decibels":
+    print("Correct! ")
+    score += 1
+else:
+    print("Incorrect! ")
+    
+    
+
+
+
+print("\n")
+print("Next Question! ")
+print("\n")
+Q_no += 1
+answer = input(f"{Q_no}. What is the 5th planet from the sun ")
+if answer.lower() == "jupiter":
+    print("Correct! ")
+    score += 1
 else:
     print("Incorrect! ")
 
 
+print(f"Your final score is: {score} out of {Q_no}")
+f_percent = score/7 * 100
+f_percent = round(f_percent)
+
+print(f"Your Percentage Score is: {f_percent}% ")
+
+print(f"Hello {name}, you had {score} as your score. 36% of people with age {age} passed and they are {gender}s.")
 
 
 #print("Hello", name, "Welcome to the quiz game!")
